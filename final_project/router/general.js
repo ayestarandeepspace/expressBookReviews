5b69c7cc04d1d9d62a97d03bef7aa35e9c395ffb
Héctor Ayestarán <hector.ayestaran@gmail.com>
2024-10-13 18:22:08 +0200

    Get book details by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -35,8 +35,23 @@ public_users.get('/isbn/:isbn',function (req, res) {
 
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  let author = req.params.author;
+  let book = null;
+
+  for(let isbn in books) {
+    if(author === books[isbn].author) {
+      book = books[isbn];
+    }
+  }
+  // Return 200 if any book with the same author is found, otherwise 404
+  if (book) {
+    return res.status(200).send(
+      JSON.stringify(book, null, 4)
+    );
+  } else {
+    return res.status(404).send('The provided author returned no book.');
+  }
 });
 
 // Get all books based on title
